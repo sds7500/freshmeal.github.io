5ae0f15fe464f4481efa7b703511c15fdfb5d007
somya <somyasheti@gmail.com>
2021-02-14 07:35:33 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,23 @@ $(document).ready(function(){
         slidesToScroll:1,
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
-        
+        responsive:[
+        {
+            breakpoint:992,
+            settings:{
+                slidesToShow:2,
+            }
+        },
+        {
+            breakpoint:768,
+            settings:{
+                slidesToShow:1,
+            }
+        }
+    ]
     });
+
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled')
+    })
 });
